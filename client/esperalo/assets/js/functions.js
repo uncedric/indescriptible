6aa218d67bf94d5855155dc81790652789e5a4f0
Cedric <cedric.luckie@gmail.com>
2016-03-24 12:20:02 -0600

    Cambiar fotos de los locutores principales

--- client/esperalo/assets/js/functions.js
+++ client/esperalo/assets/js/functions.js
@@ -55,28 +55,25 @@ jQuery(document).ready(function($) {
 	/* Time Countdown
 	-------------------------------------------------------------------*/
 	$('#time_countdown').countDown({
+      targetDate: {
+          'day': 1,
+          'month': 4,
+          'year': 2016,
+          'hour': 0,
+          'min': 0,
+          'sec': 0
+      },
+			omitWeeks: true
+	  });
 
-        // targetDate: {
-        //     'day': 30,
-        //     'month': 9,
-        //     'year': 2015,
-        //     'hour': 0,
-        //     'min': 0,
-        //     'sec': 0
-        // },
-        // omitWeeks: true
-
-         targetOffset: {
-            'day':      3,
-            'month':    0,
-            'year':     0,
-            'hour':     0,
-            'min':      0,
-            'sec':      3
-		},
-		omitWeeks: true
-
-	    });
+		// targetOffset: {
+		// 	 'day':      3,
+		// 	 'month':    0,
+		// 	 'year':     0,
+		// 	 'hour':     0,
+		// 	 'min':      0,
+		// 	 'sec':      3
+		//  },
 
 
 
